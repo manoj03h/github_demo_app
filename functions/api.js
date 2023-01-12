cfac68a95652c584aed185db5479c91fd5b4d79f
Manoj Hembram <manoj03h@gmail.com>
2023-01-12 16:19:42 +0530

    Changed some data to demo_app

--- functions/api.js
+++ functions/api.js
@@ -4,8 +4,14 @@ const app = express()
 //const port = process.env.PORT || 3000;
 const router = express.Router();
 
+const someData =  [
+{id: 1, message: "Message one"},
+{id: 2, message: "Message two"},
+{id: 3, message: "Message three"}
+];
+
 router.get('/', (req, res) => {
-  res.json({"message": "Hello World!"})
+  res.json([{"message": "Hello World!"}])
 });
 
 //app.use("/.netlify/functions/api", router);
